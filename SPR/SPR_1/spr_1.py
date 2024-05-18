05cb66a10efb511090a1deae8522e9f8b1b71afd
pk22 <115844791+KarynaPomin@users.noreply.github.com>
2024-05-18 23:09:24 +0200

    Update spr_1.py

--- SPR/SPR_1/spr_1.py
+++ SPR/SPR_1/spr_1.py
@@ -1,43 +1,42 @@
-# Metoda matoda trapezów
+import math
 
-def TrapezF(a, b, n):
-    def f(x):
-        return -x**3 + 2 * x**2 + x
-     
-    dx = (b - a) / 2
+def f(x):
+    return -x**3 + 2 * (x**3) + x
+
+def g(x):
+    return -x**3 + 2 * (x**3) + x + 1
+
+def Trapez(a, b, n, myFuction):
+    dx = (b - a) / n
     suma = 0
-
     for i in range(n):
-        suma += ((f(a + dx * i) + f(a + dx * i + dx)) * dx) / 2
-    
+        suma += (myFuction(a + dx * i) * dx + myFuction(a + dx * i + dx)) / 2
     return suma
 
-def TrapezG(a, b, n):
-    def g(x):
-        return -x**3 + 2 * x**2 + x + 1
-       
-    dx = (b - a) / 2
-    suma = 0
+# Ile worków nawozu trzeba kupić by to nawiźć nawozem do traw firmy "Nawóz do traw"?
+# 1 worek --> 5 kg ziemi
+# 1 kg ziemi --> 5 m^2
+# 1 km^2 --> 1 000 000 m^2
+# n = dzień urodzenia + 15
 
-    for i in range(n):
-        suma += ((g(a + dx * i) + g(a + dx * i + dx)) * dx) / 2
-    
-    return suma
-
-def Worki(powierzchnia_dzialki):
-    worek = 5
-    kilogramy_jednego_worka = 5**2
-
-    kilogramy = powierzchnia_dzialek / kilogramy_jednego_worka
-    ile_workow = kilogramy / worek
+def IleWorkow(powierzchnia_dzialki):
+    powierzchnia_dzialki *= 1000000 # km^2 -> m^2
+    ile_kg = powierzchnia_dzialki / 5
+    ile_workow = math.ceil(ile_kg / 5)
 
     return ile_workow
 
-dzialkaF = TrapezF(0, 2, 19)
-dzialkaG = TrapezG(0, 2, 19)
+# Dane
+a = 0
+b = 2
+n = 19
+powierzchnia_d = Trapez(a, b, n, f)
+powierzchnia_g = Trapez(a, b, n, g)
 
-powierzchnia_dzialek = dzialkaF + dzialkaG
-print(f"1. Dolna część: {dzialkaF}")
-print(f"2. Górna część: {dzialkaG}")
-print(f"3. Pod rzeką powierzchnia dziłki wynosi: {dzialkaF}")
-print(f"4. Trzeba {Worki(powierzchnia_dzialek)} worków.")
+print(f"Powierzchnia dolnej części wynosi: {powierzchnia_d}.")
+print(f"Powierzchnia górnej części wynosi: {powierzchnia_g}.")
+print(f"Powierzchnia działki pod rzeką wynosi: {powierzchnia_d}.")
+print(f"Dla uzupełniania działki potrzebujesz zakupić {IleWorkow(powierzchnia_d) + IleWorkow(powierzchnia_g)} worków.")
+
+
+
