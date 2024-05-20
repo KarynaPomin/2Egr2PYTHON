23c257be7da77590dd122b5437d6bd5e8c72d822
pk22 <115844791+KarynaPomin@users.noreply.github.com>
2024-05-20 06:17:37 +0200

    Create NewtonRaphson.py

---
+++ Algorytmy/NewtonRaphson.py
@@ -0,0 +1,22 @@
+# Algorytm Newton-Raphson - poszukiwanie pierwiastka z liczby
+def newton_raphson1(p, epsilon):
+    a = p
+    b = 1
+    while a - b > epsilon:
+        a = (a + b) / 2
+        b = p / a
+    return a
+
+print(newton_raphson1(4900, 0.000001))
+
+
+
+def newton_raphson2(p, n):
+    a = p
+    b = 1
+    for i in range(n):
+        a = (a + b) / 2
+        b = p / a
+    return a
+
+print(newton_raphson2(4900, 7))
